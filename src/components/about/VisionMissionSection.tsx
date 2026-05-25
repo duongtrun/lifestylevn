df2f948deb1c99d7dac428741a89c057fefae296
DuongTrun <125685311+DuongTrun@users.noreply.github.com>
2026-05-25 10:54:50 +0700

    style(about): reduce vertical padding in VisionMissionSection component

--- src/components/about/VisionMissionSection.tsx
+++ src/components/about/VisionMissionSection.tsx
@@ -63,7 +63,7 @@ export default function VisionMissionSection() {
   const Icon = currentTab.icon;
 
   return (
-    <section className="relative w-full py-16 lg:py-24 bg-white overflow-hidden">
+    <section className="relative w-full py-12 lg:py-16 bg-white overflow-hidden">
       <div className="container mx-auto px-4 md:px-10">
 
         {/* --- TIÊU ĐỀ SECTION --- */}
